da687970747cf468c1b6f4b2c1d6e05c93bd0a06
XinHong-uts <62465945+XinHong-uts@users.noreply.github.com>
2021-07-18 16:51:12 +1000

    Create Multivalue TDD

--- TDD/TDD_Multivalue.py
+++ TDD/TDD_Multivalue.py
@@ -29,11 +29,12 @@ class Index:
     
 class Node:
     """To define the node of TDD"""
-    def __init__(self,key):
+    def __init__(self,key,num=2):
         self.idx = 0
         self.key = key
-        self.out_weight=[1]*2
-        self.successor=[None]*2
+        self.succ_num=num
+        self.out_weight=[1]*num
+        self.successor=[None]*num
 
 
 class TDD:
@@ -45,7 +46,7 @@ class TDD:
         
         self.key_2_index=dict()
         self.index_2_key=dict()
-        
+        self.key_width=dict()
         if isinstance(node,Node):
             self.node=node
         else:
@@ -124,12 +125,12 @@ def get_node_set(node,node_set=set()):
     """Only been used when counting the node number of a TDD"""
     if not node in node_set:
         node_set.add(node)
-        for k in range(2):
+        for k in range(node.succ_num):
             if node.successor[k]:
                 node_set = get_node_set(node.successor[k],node_set)
     return node_set
 
-def Find_Or_Add_Unique_table(x,weight1=0,weight2=0,node1=None,node2=None):
+def Find_Or_Add_Unique_table(x,weigs=[],succ_nodes=[]):
     """To return a node if it already exist, creates a new node otherwise"""
     global global_node_idx,unique_table
     
@@ -141,51 +142,60 @@ def Find_Or_Add_Unique_table(x,weight1=0,weight2=0,node1=None,node2=None):
             res.idx=0
             unique_table[x]=res
         return res
-    temp_key=(x,get_int_key(weight1),get_int_key(weight2),node1.idx,node2.idx)
+    temp_key=[x]
+    for k in range(len(weigs)):
+        temp_key.append(get_int_key(weigs[k]))
+        temp_key.append(succ_nodes[k])
+    temp_key=tuple(temp_key)
     if temp_key in unique_table:
         return unique_table[temp_key]
     else:
-        res=Node(x)
+        res=Node(x,len(succ_nodes))
         global_node_idx+=1
         res.idx=global_node_idx
-        res.out_weight=[weight1,weight2]
-        res.successor=[node1,node2]
+        res.out_weight=weigs
+        res.successor=succ_nodes
         unique_table[temp_key]=res
     return res
 
 
-def normalize(x,low,high):
+def normalize(x,the_successors):
     """The normalize and reduce procedure"""
-    if high==low:
-        return high
-    weig1=low.weight
-    weig2=high.weight
-    epi=0.000001
+    all_equal=True
+    for k in range(1,len(the_successors)):
+        if the_successors[k]!=the_successors[0]:
+            all_equal=False
+            break
+    if all_equal:
+        return the_successors[0]
+    
+    weigs=[succ.weight for succ in the_successors]
 
-    if get_int_key(weig1)==(0,0) and get_int_key(weig2)==(0,0):
+    all_zeros=True
+    for k in range(len(the_successors)):
+        if get_int_key(weigs[k])!=(0,0):
+            all_zeros=False
+            break
+    if all_zeros:
         node=Find_Or_Add_Unique_table(-1)
         res=TDD(node)
         res.weight=0
         return res
-    elif get_int_key(weig1)==(0,0):
-        node=Find_Or_Add_Unique_table(-1)
-        low=TDD(node)
-        weig1=0
-    elif get_int_key(weig2)==(0,0):
-        node=Find_Or_Add_Unique_table(-1)
-        high=TDD(node)
-        weig2=0
-        
-    if np.around(abs(weig1)/epi)>=np.around(abs(weig2)/epi):
-        weig=weig1
-    else:
-        weig=weig2
+    for k in range(len(the_successors)):
+        if get_int_key(weigs[k])==(0,0):
+            node=Find_Or_Add_Unique_table(-1)
+            the_successors[k]=TDD(node)
+            the_successors[k].weight=weigs[k]=0
 
-    weig1=weig1/weig
-    weig2=weig2/weig
-    node=Find_Or_Add_Unique_table(x,weig1,weig2,low.node,high.node)
+    epi=0.000001
+    
+    weigs_abs=[np.around(abs(weig)/epi) for weig in weigs]
+    weig_max=weigs[weigs_abs.index(max(weigs_abs))]
+    weigs=[weig/weig_max for weig in weigs]
+    succ_nodes=[succ.node for succ in the_successors]
+    node=Find_Or_Add_Unique_table(x,weigs,succ_nodes)
     res=TDD(node)
-    res.weight=weig
+    res.weight=weig_max
     return res
 
 def get_count():
@@ -282,10 +292,11 @@ def diag_matrix_2_TDD(U,var):
         low.weight=U[0][0]
         high=TDD(term)
         high.weight=U[1][1]
-        res=normalize(0, low, high)
+        res=normalize(0, [low, high])
     res.index_set=var
     res.index_2_key={-1:-1,var[0].key:0}
     res.key_2_index={-1:-1,0:var[0].key}
+    res.key_width[0]=2
     return res
 
 def diag_matrix_2_TDD2(U,var):
@@ -304,11 +315,12 @@ def diag_matrix_2_TDD2(U,var):
     if low==high:
         res=low
     else:
-        res=normalize(1,low,high)
+        res=normalize(1,[low,high])
         
     res.index_set=var
     res.index_2_key={-1:-1,x0:0,x1:1}
     res.key_2_index={-1:-1,0:x0,1:x1}
+    res.key_width[0]=res.key_width[1]=2
     return res
 
 
@@ -346,6 +358,7 @@ def cnot_2_TDD(var,case=1):
         res.index_set=[var[0],var[2],var[3],var[4]]
     else:
         res.index_set=[var[1],var[3],var[4]]
+    res.key_width[0]=res.key_width[1]=res.key_width[2]=2
     return res
 
 def Two_qubit_gate_2TDD(U,var):
@@ -374,6 +387,7 @@ def Two_qubit_gate_2TDD(U,var):
     res.index_2_key=idx_2_key
     res.key_2_index=key_2_idx
     res.index_set=[var[0],var[1],var[2],var[3]]
+    res.key_width[0]=res.key_width[1]=res.key_width[2]=res.key_width[3]=2
     return res
     
 def get_tdd(U,var,idx_2_key):
@@ -394,14 +408,15 @@ def get_tdd(U,var,idx_2_key):
     split_U=np.split(U,2,min_pos)
     low=get_tdd(split_U[0],new_var,idx_2_key)
     high=get_tdd(split_U[1],new_var,idx_2_key)
-    tdd = normalize(idx_2_key[x], low, high)
+    tdd = normalize(idx_2_key[x], [low,high])
     return tdd
 
     
 def np_2_tdd(U,split_pos=None):
     #index is the index_set as the axis order of the matrix
     U_dim=U.ndim
-    if sum(U.shape)==U_dim:
+    U_shape=U.shape
+    if sum(U_shape)==U_dim:
         node=Find_Or_Add_Unique_table(-1)
         res=TDD(node)
         for k in range(U_dim):
@@ -410,10 +425,15 @@ def np_2_tdd(U,split_pos=None):
         return res
     if split_pos==None:
         split_pos=U_dim-1
-    split_U=np.split(U,2,split_pos)
-    low=np_2_tdd(split_U[0],split_pos-1)
-    high=np_2_tdd(split_U[1],split_pos-1)
-    tdd = normalize(split_pos, low, high)
+        
+    split_U=np.split(U,U_shape[split_pos],split_pos)
+    the_successors=[]
+    for k in range(U_shape[split_pos]):
+        res=np_2_tdd(split_U[k],split_pos-1)
+        the_successors.append(res)
+    tdd = normalize(split_pos,the_successors)
+    for k in range(len(U_shape)):
+        tdd.key_width[k]=U_shape[k]
     return tdd
     
 def tdd_2_np(tdd,split_pos=None):    
@@ -422,11 +442,14 @@ def tdd_2_np(tdd,split_pos=None):
     if split_pos==-1:
         return tdd.weight
     else:
-        low=Slicing(tdd,split_pos,0)
-        res0=tdd_2_np(low,split_pos-1)
-        high=Slicing(tdd,split_pos,1)
-        res1=tdd_2_np(high,split_pos-1)
-        res=tdd.weight*np.stack((res0, res1), axis=split_pos)
+        the_succs=[]
+        for k in range(tdd.key_width[split_pos]):
+            succ=Slicing(tdd,split_pos,k)
+            succ.key_width=tdd.key_width
+            temp_res=tdd_2_np(succ,split_pos-1)
+            the_succs.append(temp_res)
+        res=np.stack(tuple(the_succs), axis=split_pos)
+        res=res*tdd.weight
         return res
     
     
@@ -580,28 +603,59 @@ def contract(tdd1,tdd2,key_2_new_key,cont_order,cont_num):
                 
     if cont_order[0][k1]<cont_order[1][k2]:
         the_key=key_2_new_key[0][k1]
-        s0=k1
-        s1=float('inf')
+        if the_key!='c':
+            the_successors=[]
+            for k in range(tdd1.node.succ_num):
+                res=contract(Slicing(tdd1,k1,k),tdd2,key_2_new_key,cont_order,cont_num)
+                the_successors.append(res)
+            tdd=normalize(the_key,the_successors)
+            insert_2_computed_table(['*',tdd1,tdd2,temp_key_2_new_key,cont_num],tdd)
+            tdd.weight=tdd.weight*w1*w2
+        else:
+            tdd=TDD(Find_Or_Add_Unique_table(-1))
+            tdd.weight=0
+            for k in range(tdd1.node.succ_num):
+                res=contract(Slicing(tdd1,k1,k),tdd2,key_2_new_key,cont_order,cont_num-1)           
+                tdd=add(tdd,res)
+            insert_2_computed_table(['*',tdd1,tdd2,temp_key_2_new_key,cont_num],tdd)
+            tdd.weight=tdd.weight*w1*w2
     elif cont_order[0][k1]==cont_order[1][k2]:
         the_key=key_2_new_key[0][k1]
-        s0=k1
-        s1=k2
+        if the_key!='c':
+            the_successors=[]
+            for k in range(tdd1.node.succ_num):
+                res=contract(Slicing(tdd1,k1,k),Slicing(tdd2,k2,k),key_2_new_key,cont_order,cont_num)
+                the_successors.append(res)
+            tdd=normalize(the_key,the_successors)
+            insert_2_computed_table(['*',tdd1,tdd2,temp_key_2_new_key,cont_num],tdd)
+            tdd.weight=tdd.weight*w1*w2
+        else:
+            tdd=TDD(Find_Or_Add_Unique_table(-1))
+            tdd.weight=0
+            for k in range(tdd1.node.succ_num):
+                res=contract(Slicing(tdd1,k1,k),Slicing(tdd2,k2,k),key_2_new_key,cont_order,cont_num-1)           
+                tdd=add(tdd,res)
+            insert_2_computed_table(['*',tdd1,tdd2,temp_key_2_new_key,cont_num],tdd)
+            tdd.weight=tdd.weight*w1*w2
     else:
         the_key=key_2_new_key[1][k2]
-        s0=float('inf')
-        s1=k2
-    if the_key!='c':
-        low=contract(Slicing(tdd1,s0,0),Slicing(tdd2,s1,0),key_2_new_key,cont_order,cont_num)
-        high=contract(Slicing(tdd1,s0,1),Slicing(tdd2,s1,1),key_2_new_key,cont_order,cont_num)
-        tdd=normalize(the_key,low,high)
-        insert_2_computed_table(['*',tdd1,tdd2,temp_key_2_new_key,cont_num],tdd)
-        tdd.weight=tdd.weight*w1*w2
-    else:
-        low=contract(Slicing(tdd1,s0,0),Slicing(tdd2,s1,0),key_2_new_key,cont_order,cont_num-1)
-        high=contract(Slicing(tdd1,s0,1),Slicing(tdd2,s1,1),key_2_new_key,cont_order,cont_num-1)
-        tdd=add(low,high)
-        insert_2_computed_table(['*',tdd1,tdd2,temp_key_2_new_key,cont_num],tdd)
-        tdd.weight=tdd.weight*w1*w2
+        if the_key!='c':
+            the_successors=[]
+            for k in range(tdd2.node.succ_num):
+                res=contract(tdd1,Slicing(tdd2,k2,k),key_2_new_key,cont_order,cont_num)
+                the_successors.append(res)
+            tdd=normalize(the_key,the_successors)
+            insert_2_computed_table(['*',tdd1,tdd2,temp_key_2_new_key,cont_num],tdd)
+            tdd.weight=tdd.weight*w1*w2
+        else:
+            tdd=TDD(Find_Or_Add_Unique_table(-1))
+            tdd.weight=0
+            for k in range(tdd2.node.succ_num):
+                res=contract(tdd1,Slicing(tdd2,k2,k),key_2_new_key,cont_order,cont_num-1)           
+                tdd=add(tdd,res)
+            insert_2_computed_table(['*',tdd1,tdd2,temp_key_2_new_key,cont_num],tdd)
+            tdd.weight=tdd.weight*w1*w2
+
     return tdd
     
 def Slicing(tdd,x,c):
@@ -670,14 +724,23 @@ def add(tdd1,tdd2):
         
     if find_computed_table(['+',tdd1,tdd2]):
         return find_computed_table(['+',tdd1,tdd2])
-    
-    if k1>=k2:
+    the_successors=[]
+    if k1>k2:
         x=k1
+        for k in range(tdd1.node.succ_num):
+            res=add(Slicing2(tdd1,x,k),tdd2)
+            the_successors.append(res)
+    elif k1==k2:
+        x=k1
+        for k in range(tdd1.node.succ_num):
+            res=add(Slicing2(tdd1,x,k),Slicing2(tdd2,x,k))
+            the_successors.append(res)        
     else:
         x=k2
-        
-    low=add(Slicing2(tdd1,x,0),Slicing2(tdd2,x,0))
-    high=add(Slicing2(tdd1,x,1),Slicing2(tdd2,x,1))
-    res = normalize(x,low,high)
+        for k in range(tdd2.node.succ_num):
+            res=add(tdd1,Slicing2(tdd2,x,k))
+            the_successors.append(res)
+            
+    res = normalize(x,the_successors)
     insert_2_computed_table(['+',tdd1,tdd2],res)
     return res
