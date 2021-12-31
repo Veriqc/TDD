8cdb51e6a7061112739bc4bf73e6ce20b1548b08
Veriqc <62465945+Veriqc@users.noreply.github.com>
2021-12-31 10:11:30 +0800

    Add files via upload

---
+++ TDD/TDD_Q.py
@@ -0,0 +1,328 @@
+import numpy as np
+from TDD.TN import Index,Tensor,TensorNetwork
+from qiskit.quantum_info.operators import Operator
+import time
+
+def is_diagonal(U):
+    i, j = np.nonzero(U)
+    return np.all(i == j)
+
+def add_hyper_index(var_list,hyper_index):
+    for var in var_list:
+        if not var in hyper_index:
+            hyper_index[var]=0
+            
+def reshape(U):
+    if U.shape==(2,2):
+        return U
+    
+    if U.shape[0]==U.shape[1]:
+        split_U=np.split(U,2,1)
+    else:
+        split_U=np.split(U,2,0)
+    split_U[0]=reshape(split_U[0])
+    split_U[1]=reshape(split_U[1]) 
+    return np.array([split_U])[0]            
+            
+def get_real_qubit_num(cir):
+    """Calculate the real number of qubits of a circuit"""
+    gates=cir.data
+    q=0
+    for k in range(len(gates)):
+        q=max(q,max([qbit.index for qbit in gates[k][1]]))
+    return q+1
+
+def cir_2_tn(cir,input_s=[],output_s=[]):
+    """return the dict that link every quantum gate to the corresponding index"""
+#     print(1)
+#     t=time.time()
+    
+    
+    hyper_index=dict()
+    qubits_index = dict()
+    start_tensors= dict()
+    end_tensors = dict()
+    
+    qubits_num=get_real_qubit_num(cir)
+
+    for k in range(qubits_num):
+        qubits_index[k]=0
+        
+    tn=TensorNetwork([],tn_type='cir',qubits_num=qubits_num)
+
+        
+    if input_s:
+        U0=np.array([1,0])
+        U1=np.array([0,1])
+        for k in range(qubits_num):
+            if input_s[k]==0:
+                ts=Tensor(U0,[Index('x'+str(k))],'in',[k])
+            elif input_s[k]==1:
+                ts=Tensor(U1,[Index('x'+str(k))],'in',[k])
+            else:
+                print('Only support computational basis input')
+            tn.tensors.append(ts)
+                
+    gates=cir.data
+    for k in range(len(gates)):
+        g=gates[k]
+        nam=g[0].name
+        q = [q.index for q in g[1]]
+        var=[]
+        if nam=='cx':
+#             print(Operator(g[0]).data)
+            var_con='x'+ str(q[0])+'_'+str(qubits_index[q[0]])
+            var_tar_in='x'+ str(q[1])+'_'+str(qubits_index[q[1]])
+            var_tar_out='x'+ str(q[1])+'_'+str(qubits_index[q[1]]+1)
+            add_hyper_index([var_con,var_tar_in,var_tar_out],hyper_index)
+            var+=[Index(var_con,hyper_index[var_con]),Index(var_con,hyper_index[var_con]+1),Index(var_tar_in,hyper_index[var_tar_in]),Index(var_tar_out,hyper_index[var_tar_out])]
+            U=np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]])
+            U=reshape(U)
+            ts=Tensor(U,var,nam,q)
+            tn.tensors.append(ts)
+            if qubits_index[q[0]]==0 and hyper_index[var_con]==0:
+                start_tensors[q[0]]=ts
+            if qubits_index[q[1]]==0 and hyper_index[var_tar_in]==0:
+                start_tensors[q[1]]=ts
+            end_tensors[q[0]]=ts
+            end_tensors[q[1]]=ts
+            hyper_index[var_con]+=1
+            qubits_index[q[1]]+=1            
+            continue
+        ts=Tensor([],[],nam,q)
+        U=Operator(g[0]).data
+        if is_diagonal(U):
+            for k in q:
+                var_in='x'+ str(k)+'_'+str(qubits_index[k])
+                add_hyper_index([var_in],hyper_index)
+                var+=[Index(var_in,hyper_index[var_in]),Index(var_in,hyper_index[var_in]+1)]
+                if qubits_index[k]==0 and hyper_index[var_in]==0:
+                    start_tensors[k]=ts
+                end_tensors[k]=ts             
+                hyper_index[var_in]+=1
+        else:
+            for k in q:
+                var_in='x'+ str(k)+'_'+str(qubits_index[k])
+                var_out='x'+ str(k)+'_'+str(qubits_index[k]+1)
+                add_hyper_index([var_in,var_out],hyper_index)
+                var+=[Index(var_in,hyper_index[var_in]),Index(var_out,hyper_index[var_out])]
+                if qubits_index[k]==0 and hyper_index[var_in]==0:
+                    start_tensors[k]=ts
+                end_tensors[k]=ts                
+                qubits_index[k]+=1
+        if len(q)>1:
+            U=reshape(U)
+        ts.data=U
+        ts.index_set=var
+        tn.tensors.append(ts)
+
+#     print(2)
+#     print(time.time()-t)
+
+#     for k in range(qubits_num):
+#         last1=Index('x'+str(k)+'_'+str(0),0)
+#         new1=Index('x'+str(k),0)
+#         last2=Index('x'+str(k)+'_'+str(qubits_index[k]),hyper_index['x'+str(k)+'_'+str(qubits_index[k])])
+#         new2=Index('y'+str(k),0)
+#         for ts in tn.tensors:
+#             ts.index_set=[new1 if item==last1 else item for item in ts.index_set]
+#             ts.index_set=[new2 if item==last2 else item for item in ts.index_set]
+
+    for k in range(qubits_num):
+        last1=Index('x'+str(k)+'_'+str(0),0)
+        new1=Index('x'+str(k),0)
+        start_tensors[k].index_set[start_tensors[k].index_set.index(last1)]=new1
+        last2=Index('x'+str(k)+'_'+str(qubits_index[k]),hyper_index['x'+str(k)+'_'+str(qubits_index[k])])
+        new2=Index('y'+str(k),0)
+        end_tensors[k].index_set[end_tensors[k].index_set.index(last2)]=new2
+               
+#     print(3)
+#     print(time.time()-t)
+    if output_s:
+        U0=np.array([1,0])
+        U1=np.array([0,1])
+        for k in range(qubits_num):
+            if input_s[k]==0:
+                ts=Tensor(U0,[Index('y'+str(k))],'out',[k])
+            elif input_s[k]==1:
+                ts=Tensor(U1,[Index('y'+str(k))],'out',[k])
+            else:
+                print('Only support computational basis output')
+            tn.tensors.append(ts)
+    
+    all_indexs=[]
+    for k in range(qubits_num):
+        all_indexs.append('x'+str(k))
+        for k1 in range(qubits_index[k]+1):
+            all_indexs.append('x'+str(k)+'_'+str(k1))
+        all_indexs.append('y'+str(k))
+#     print(4)
+#     print(time.time()-t)
+    return tn,all_indexs
+
+def add_inputs(tn,input_s,qubits_num):
+    U0=np.array([1,0])
+    U1=np.array([0,1])
+    if len(input_s)!= qubits_num:
+        print("inputs is not match qubits number")
+        return 
+    for k in range(qubits_num-1,-1,-1):
+        if input_s[k]==0:
+            ts=Tensor(U0,[Index('x'+str(k))],'in',[k])
+        elif input_s[k]==1:
+            ts=Tensor(U1,[Index('x'+str(k))],'in',[k])
+        else:
+            print('Only support computational basis input')
+        tn.tensors.insert(0,ts)
+            
+def add_outputs(tn,output_s,qubits_num):
+    U0=np.array([1,0])
+    U1=np.array([0,1])
+    if len(output_s)!= qubits_num:
+        print("outputs is not match qubits number")
+        return 
+    for k in range(qubits_num):
+        if output_s[k]==0:
+            ts=Tensor(U0,[Index('y'+str(k))],'out',[k])
+        elif output_s[k]==1:
+            ts=Tensor(U1,[Index('y'+str(k))],'out',[k])
+        else:
+            print('Only support computational basis output')
+        tn.tensors.append(ts)       
+
+def add_trace_line(tn,qubits_num):
+    U=np.eye(2)
+    for k in range(qubits_num-1,-1,-1):
+        var_in='x'+str(k)
+        var=[Index('x'+str(k),0),Index('y'+str(k),0)]
+        ts=Tensor(U,var,'tr',[k])
+        tn.tensors.insert(0,ts)
+        
+# def Single_qubit_gate_2TDD(U,var):
+#     """Get the TDD of a 2*2 matrix, here var = [column index, row index]"""
+#     idx_2_key={-1:-1}
+#     key_2_idx={-1:-1}
+#     if var[0]<var[1]:
+#         idx_2_key[var[0].key]=1
+#         idx_2_key[var[1].key]=0
+#         key_2_idx[0]=var[1].key
+#         key_2_idx[1]=var[0].key
+#     else:
+#         idx_2_key[var[0].key]=0
+#         idx_2_key[var[1].key]=1
+#         key_2_idx[0]=var[0].key
+#         key_2_idx[1]=var[1].key 
+    
+#     res=get_tdd(U,[var[1],var[0]],idx_2_key) 
+#     res.index_set=var
+#     res.index_2_key=idx_2_key
+#     res.key_2_index=key_2_idx
+#     return res 
+
+# def diag_matrix_2_TDD(U,var):
+#     term=Find_Or_Add_Unique_table(-1)
+#     if get_int_key(U[0][0])==get_int_key(U[1][1]):
+#         res=TDD(term)
+#         res.weight=U[0][0]
+#     else:
+#         low=TDD(term)
+#         low.weight=U[0][0]
+#         high=TDD(term)
+#         high.weight=U[1][1]
+#         res=normalize(0, [low, high])
+#     res.index_set=var
+#     res.index_2_key={-1:-1,var[0].key:0}
+#     res.key_2_index={-1:-1,0:var[0].key}
+#     res.key_width[0]=2
+#     return res
+
+# def diag_matrix_2_TDD2(U,var):
+#     if var[0]<var[2]:
+#         x1=var[0].key
+#         x0=var[2].key
+#         low = diag_matrix_2_TDD(U[:2,:2],var[2:])
+#         high = diag_matrix_2_TDD(U[2:,2:],var[2:])          
+#     else:
+#         x1=var[2].key
+#         x0=var[0].key
+#         low = diag_matrix_2_TDD(np.diag([U[0][0],U[2,2]]),var[:2])
+#         high = diag_matrix_2_TDD(np.diag([U[1][1],U[3,3]]),var[:2])
+       
+    
+#     if low==high:
+#         res=low
+#     else:
+#         res=normalize(1,[low,high])
+        
+#     res.index_set=var
+#     res.index_2_key={-1:-1,x0:0,x1:1}
+#     res.key_2_index={-1:-1,0:x0,1:x1}
+#     res.key_width[0]=res.key_width[1]=2
+#     return res
+
+
+# def cnot_2_TDD(var,case=1):
+    
+#     if case==2:
+#         term=Find_Or_Add_Unique_table(-1)
+#         res=TDD(term)
+#         res.index_set = [var[0],var[1],var[2]]
+#         res.index_2_key={-1:-1,var[0].key:0}
+#         res.key_2_index={-1:-1,0:var[0].key}
+#         return res
+#     XOR=np.zeros((2,2,2))
+#     XOR[0][0][0]=XOR[0][1][1]=XOR[1][0][1]=XOR[1][1][0]=1
+    
+#     idx_2_key={-1:-1}
+#     key_2_idx={-1:-1}
+    
+#     new_var=[var[0],var[3],var[4]]
+#     max_var=max(new_var)
+#     idx_2_key[max_var.key]=0
+#     key_2_idx[0]=max_var.key
+#     new_var.remove(max_var)
+#     max_var=max(new_var)
+#     idx_2_key[max_var.key]=1
+#     key_2_idx[1]=max_var.key
+#     new_var.remove(max_var)
+#     max_var=max(new_var)
+#     idx_2_key[max_var.key]=2
+#     key_2_idx[2]=max_var.key
+#     res=get_tdd(XOR,[var[0],var[3],var[4]],idx_2_key)
+#     res.index_2_key=idx_2_key
+#     res.key_2_index=key_2_idx
+#     if case==1:
+#         res.index_set=[var[0],var[2],var[3],var[4]]
+#     else:
+#         res.index_set=[var[1],var[3],var[4]]
+#     res.key_width[0]=res.key_width[1]=res.key_width[2]=2
+#     return res
+
+# def Two_qubit_gate_2TDD(U,var):
+#     """Get the TDD of a 2*2 matrix, here var = [column index, row index]"""
+#     U=U.reshape(2,2,2,2)
+#     idx_2_key={-1:-1}
+#     key_2_idx={-1:-1}
+    
+#     new_var=[var[0],var[1],var[2],var[3]]
+#     max_var=max(new_var)
+#     idx_2_key[max_var.key]=0
+#     key_2_idx[0]=max_var.key
+#     new_var.remove(max_var)
+#     max_var=max(new_var)
+#     idx_2_key[max_var.key]=1
+#     key_2_idx[1]=max_var.key
+#     new_var.remove(max_var)
+#     max_var=max(new_var)
+#     idx_2_key[max_var.key]=2
+#     key_2_idx[2]=max_var.key
+#     new_var.remove(max_var)
+#     max_var=max(new_var)
+#     idx_2_key[max_var.key]=3
+#     key_2_idx[3]=max_var.key
+#     res=get_tdd(U,[var[3],var[1],var[2],var[0]],idx_2_key) 
+#     res.index_2_key=idx_2_key
+#     res.key_2_index=key_2_idx
+#     res.index_set=[var[0],var[1],var[2],var[3]]
+#     res.key_width[0]=res.key_width[1]=res.key_width[2]=res.key_width[3]=2
+#     return res
---
+++ TDD/TN.py
@@ -0,0 +1,286 @@
+import numpy as np
+from TDD.TDD import Index,get_tdd,get_identity_tdd,cont
+import networkx as nx
+from networkx.algorithms.approximation.treewidth import treewidth_min_degree,treewidth_min_fill_in
+
+common_tensor_table = dict()
+
+class Tensor:
+    def __init__(self,data=[],index=[],name=None,qubits=None):
+        self.data=data
+        self.index_set=index
+        self.name=name
+        self.qubits=qubits #This is used only when it represent a quantum gate 
+        
+    def tdd(self):
+        return get_tdd(self.data,self.index_set)        
+        
+class TensorNetwork:
+    def __init__(self,tensors=dict(),tn_type='tn',qubits_num=0):
+        self.tensors=tensors
+        self.tn_type=tn_type
+        self.index_set=set()
+        self.index_2_tensor=dict()
+        self.qubits_num=qubits_num #This is used only when it represent a quantum circuit
+#         if len(data)>0 and len(index_set)==0:
+#             self.index_set=self.get_index_set()
+#         if len(data)>0 and len(index_2_node)==0:
+#             self.index_2_node=self.get_index_2_node()
+    def cont(self,optimizer=None):
+        tdd=get_identity_tdd()        
+        if optimizer=='tree_decomposition':
+            decom_tree,tree_width=get_tree_decomposition(self)
+            cont_index = find_contraction_index(decom_tree)
+            computed_tdd_list=[]
+            while cont_index:
+                computed_tdd_list = contract_an_index(self,cont_index,computed_tdd_list)
+                cont_index=find_contraction_index(decom_tree)
+            
+            for temp_tdd in computed_tdd_list:
+                tdd=cont(tdd,temp_tdd)
+            return tdd
+        if optimizer=='cir_partition1':
+            if not self.tn_type=='cir':
+                print("This optimizer is only used for quantum circuits!")
+                return tdd
+            partion_cir=circuit_partion1(self)
+            for level in range(len(partion_cir)):
+                temp_tn=TensorNetwork(partion_cir[level][0])
+                tdd1=temp_tn.cont()
+                temp_tn=TensorNetwork(partion_cir[level][1])
+                tdd2=temp_tn.cont()
+                temp_tdd=cont(tdd1,tdd2)
+                tdd=cont(tdd,temp_tdd)
+            return tdd
+        
+        if optimizer=='cir_partition2':
+            if not self.tn_type=='cir':
+                print("This optimizer is only used for quantum circuits!")
+                return tdd
+            partion_cir=circuit_partion2(self)
+            
+            for level in range(len(partion_cir)):
+                temp_tn=TensorNetwork(partion_cir[level][0])
+                tdd1=temp_tn.cont()
+                temp_tn=TensorNetwork(partion_cir[level][1])
+                tdd2=temp_tn.cont()
+                temp_tdd=cont(tdd1,tdd2)
+                temp_tn=TensorNetwork(partion_cir[level][2])
+                tdd3=temp_tn.cont()            
+                temp_tdd=cont(tdd3,temp_tdd)
+                tdd=cont(tdd,temp_tdd)
+            return tdd        
+        
+        for ts in self.tensors:
+            temp_tdd=ts.tdd()
+            tdd=cont(tdd,temp_tdd)
+        return tdd
+
+    def get_index_set(self):
+        for ts in self.tensors:
+            temp_index=[idx.key for idx in ts.index_set]
+            for idx in temp_index:
+                self.index_set.add(idx)
+                if not idx in self.index_2_tensor:
+                    self.index_2_tensor[idx]=set()
+                self.index_2_tensor[idx].add(ts)
+
+    
+#     def get_index_2_node(self):
+#         index_2_node=dict()
+#         for k in range(len(data)):
+#             temp_index=[idx.key for idx in self.data[k].index]
+#             for idx in temp_index:
+#                 if not idx in index_2_node:
+#                     index_2_node[idx]=set()
+#                 index_2_node[idx].add(k)
+#         return index_2_node
+    
+    
+def get_tree_decomposition(tn):
+    lin_graph=nx.Graph()
+    if not tn.index_set:
+        tn.get_index_set()
+    
+    lin_graph.add_nodes_from(tn.index_set)
+    
+    for ts in tn.tensors:    
+        for k1 in range(len(ts.index_set)):
+            for k2 in range(k1+1,len(ts.index_set)):
+                if ts.index_set[k1].key!=ts.index_set[k2].key:
+                    lin_graph.add_edge(ts.index_set[k1].key,ts.index_set[k2].key)
+        
+    tree_width,de_graph=treewidth_min_fill_in(lin_graph)
+#     print('The treewidth is',tree_width)
+    return de_graph,tree_width
+
+def find_contraction_index(tree_decomposition):
+    idx=None
+    if len(tree_decomposition.nodes)==1:
+        nod=[k for k in tree_decomposition.nodes][0]
+        if len(nod)!=0:
+            idx=[idx for idx in nod][0]
+            nod_temp=set(nod)
+            nod_temp.remove(idx)
+            tree_decomposition.add_node(frozenset(nod_temp))
+            tree_decomposition.remove_node(nod)
+        return idx
+    nod=0
+    for k in tree_decomposition.nodes:
+        if nx.degree(tree_decomposition)[k]==1:
+            nod=k
+            break
+            
+    neib=[k for k in tree_decomposition.neighbors(nod)][0]
+    for k in nod:
+        if not k in neib:
+            idx=k
+            break
+    if idx:     
+        nod_temp=set(nod)
+        nod_temp.remove(idx)
+        tree_decomposition.remove_node(nod)
+        if frozenset(nod_temp)!=neib:
+            tree_decomposition.add_node(frozenset(nod_temp))
+            tree_decomposition.add_edge(frozenset(nod_temp),neib)
+        return idx
+    else:
+        tree_decomposition.remove_node(nod)
+        return find_contraction_index(tree_decomposition)
+
+    
+def contract_an_index(tn,cont_index,computed_tdd_list):
+    temp_tn=TensorNetwork(tn.index_2_tensor[cont_index])
+    temp_tdd=temp_tn.cont()
+    
+    for ts in tn.index_2_tensor[cont_index]:
+        for idx in ts.index_set:
+            if idx.key!=cont_index:
+                if ts in tn.index_2_tensor[idx.key]:
+                    tn.index_2_tensor[idx.key].remove(ts)
+    temp_computed_tdd_list = []
+    for tdd in computed_tdd_list:
+        tdd_idx_out=[k.key for k in tdd.index_set]
+
+        if cont_index in tdd_idx_out:
+            temp_tdd=cont(tdd,temp_tdd)
+        else:
+            temp_computed_tdd_list.append(tdd)
+    temp_computed_tdd_list.append(temp_tdd)
+    computed_tdd_list = temp_computed_tdd_list
+    return computed_tdd_list            
+            
+def circuit_partion1(tn):
+    """The first partition scheme; 
+    cx_max is the number of CNOTs allowed to be cut
+    """
+    res=[[[],[]]]
+    
+    num_qubit=tn.qubits_num
+    
+    cx_max=num_qubit//2
+    cx_num=0
+    level=0
+        
+    qubits = []    
+    qubits.append([k for k in range(num_qubit//2)])
+    qubits.append([k for k in range(num_qubit//2,num_qubit)])
+
+    for ts in tn.tensors:
+        q = ts.qubits
+        if max(q) in qubits[0]:
+            res[level][0].append(ts)
+        elif min(q) in qubits[1]:
+            res[level][1].append(ts)
+        else:
+            cx_num+=1
+            if cx_num<=cx_max:
+                if q[-1] in qubits[0]:
+                    res[level][0].append(ts)
+                else:
+                    res[level][1].append(ts)
+            else:
+                level+=1
+                res.append([])
+                res[level].append([])
+                res[level].append([])
+                if q[-1] in qubits[0]:
+                    res[level][0].append(ts)
+                else:
+                    res[level][1].append(ts)
+                cx_num=1
+                
+#     print('circuit blocks:',2*(level+1))
+    return res            
+            
+def circuit_partion2(tn):
+    """The first partition scheme; 
+    cx_max is the number of CNOTs allowed to be cut
+    """
+    res=[[[],[],[]]]
+    
+    cx_num=0
+    num_qubit=tn.qubits_num
+    cx_max = num_qubit//2
+    c_part_width = num_qubit//2+1
+    level=0
+    
+    qubits=[]
+    qubits.append([k for k in range(num_qubit//2)])
+    qubits.append([k for k in range(num_qubit//2,num_qubit)])
+    qubits.append([])
+    c_range=[num_qubit,0]
+    
+    for ts in tn.tensors:
+        q = ts.qubits
+        
+        
+        if max(q) in qubits[0]:
+            res[level][0].append(ts)
+        elif min(q) in qubits[1]:
+            res[level][1].append(ts)
+        elif min(q) in qubits[2] and max(q) in qubits[2]:        
+            res[level][2].append(ts)
+        else:
+            if cx_num < cx_max:
+                cx_num+=1
+                if q[-1] in qubits[0]:
+                    res[level][0].append(ts)
+                else:
+                    res[level][1].append(ts)
+            else:
+                c_width = max(c_range[1],max(q))-min(c_range[0],min(q))+1
+                if c_width < c_part_width:
+                    res[level][2].append(ts)
+                    c_range[0]=min(c_range[0],min(q))
+                    c_range[1]=max(c_range[1],max(q))
+                    qubits[0]=[k for k in range(0,c_range[0])]
+                    qubits[1]=[k for k in range(c_range[1]+1,num_qubit)]
+                    qubits[2]=[k for k in range(c_range[0],c_range[1]+1)]
+                else:
+                    level+=1
+                    res.append([])
+                    res[level].append([])
+                    res[level].append([])
+                    res[level].append([])
+                    qubits.clear()
+                    qubits.append([k for k in range(num_qubit//2)])
+                    qubits.append([k for k in range(num_qubit//2,num_qubit)])
+                    qubits.append([])
+                    c_range=[num_qubit,0]
+                    
+                    if max(q) in qubits[0]:
+                        res[level][0].append(ts)
+                        cx_num=0
+                    elif min(q) in qubits[1]:
+                        res[level][1].append(ts)
+                        cx_num=0
+                    else:
+                        if q[-1] in qubits[0]:
+                            res[level][0].append(ts)
+                        else:
+                            res[level][1].append(ts)
+                        cx_num=1
+            
+#     print('circuit blocks:',3*(level+1))
+    return res
